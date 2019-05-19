07256c751aae1e5cdaec49db64d6d068f27ca352
soeyahtet <soeyahtet@192.168.0.100>
2019-05-19 16:29:35 +0800

    index.html and Application class with admin user creation.

--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/JavalapsTeam9Application.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/JavalapsTeam9Application.java
@@ -1,13 +1,30 @@
 package sg.edu.nus.javalapsteam9;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavalapsTeam9Application {
+import sg.edu.nus.javalapsteam9.enums.Roles;
+import sg.edu.nus.javalapsteam9.model.User;
+import sg.edu.nus.javalapsteam9.repo.UserRepository;
 
+@SpringBootApplication
+public class JavalapsTeam9Application implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavalapsTeam9Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		User admin = new User("admin1", "Suria", "R Asai", "12345",
+				"suria@gmail.com", Roles.AMDIN, 14, 60, "");
+		userRepo.save(admin);
 	}
 
 }
