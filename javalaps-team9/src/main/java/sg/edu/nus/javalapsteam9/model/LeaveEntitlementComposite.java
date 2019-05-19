0268a00c7d7895256fcd5f61e92963ae70966119
Ravi <ravindrab749@gmail.com>
2019-05-19 16:07:19 +0800

    changes

--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/model/LeaveEntitlementComposite.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/model/LeaveEntitlementComposite.java
@@ -36,4 +36,29 @@ public class LeaveEntitlementComposite implements Serializable {
 		this.leaveType = leaveType;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((leaveType == null) ? 0 : leaveType.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LeaveEntitlementComposite other = (LeaveEntitlementComposite) obj;
+		if (leaveType != other.leaveType)
+			return false;
+		if (role != other.role)
+			return false;
+		return true;
+	}
+
 }
