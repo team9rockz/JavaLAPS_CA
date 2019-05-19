06f5da054ed230456e2bd4eedcbc741eb5d4a166
Ravi <ravindrab749@gmail.com>
2019-05-19 16:17:10 +0800

    construction in enums

--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/enums/LeaveType.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/enums/LeaveType.java
@@ -2,8 +2,18 @@ package sg.edu.nus.javalapsteam9.enums;
 
 public enum LeaveType {
 	
-	ANNUAL,
-	MEDICAL,
-	COMPENSATION;
+	ANNUAL("ANNUAL"),
+	MEDICAL("MEDICAL"),
+	COMPENSATION("COMPENSATION");
+	
+	private String type;
+	
+	private LeaveType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
 
 }
--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/enums/Roles.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/enums/Roles.java
@@ -2,8 +2,18 @@ package sg.edu.nus.javalapsteam9.enums;
 
 public enum Roles {
 
-	AMDIN,
-	MANAGER,
-	STAFF
+	AMDIN("ADMIN"),
+	MANAGER("MANAGER"),
+	STAFF("STAFF");
+	
+	private String role;
+	
+	private Roles(String role) {
+		this.role = role;
+	}
+	
+	public String getRole() {
+		return role;
+	}
 
 }
