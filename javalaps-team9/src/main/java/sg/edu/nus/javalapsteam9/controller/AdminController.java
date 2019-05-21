ff1dee2aaa77fabbf22cd5a4f61fa31c55f3f8eb
soeyahtet <soeyahtet@172.17.172.72>
2019-05-21 11:25:50 +0800

    Some class and html files for Admin's user creation process.

--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/controller/AdminController.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/controller/AdminController.java
@@ -1,11 +1,18 @@
 package sg.edu.nus.javalapsteam9.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import sg.edu.nus.javalapsteam9.enums.Roles;
+import sg.edu.nus.javalapsteam9.model.User;
 import sg.edu.nus.javalapsteam9.service.AdminService;
 
 @Controller
@@ -15,10 +22,25 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 
-	@GetMapping("")
+	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("msg", "Admin");
+		List<User> users = new ArrayList<User>();
+		users = adminService.getAllUsers();
+		model.addAttribute("users", users);
 		return "admin/home";
 	}
+	
+	@RequestMapping(path = "/add", method = RequestMethod.GET)
+	public String addUser(Model model) {
+		
+		model.addAttribute("user", new User());
+		model.addAttribute("managers", adminService.getAllManagers());
+		return "admin/user_form";
+	}
 
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public String saveProduct(User user) {
+        adminService.createUser(user);
+        return "redirect:/admin/home";
+    }
 }
--- javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/service/AdminService.java
+++ javalaps-team9/src/main/java/sg/edu/nus/javalapsteam9/service/AdminService.java
@@ -1,8 +1,12 @@
 package sg.edu.nus.javalapsteam9.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sg.edu.nus.javalapsteam9.enums.Roles;
 import sg.edu.nus.javalapsteam9.model.User;
 import sg.edu.nus.javalapsteam9.repo.UserRepository;
 
@@ -13,6 +17,25 @@ public class AdminService {
 	private UserRepository userRepo;
 	
 	public void createUser(User user) {
+		user.setPassword(null == user.getPassword()? "12345" : user.getPassword());
 		userRepo.save(user);
+	}
+	
+	public List<User> getAllUsers(){
+		
+		List<User> users = userRepo.findAll();
+		return users;
+	}
+	
+	public List<String> getAllManagers(){
+		
+		List<User> users = userRepo.findAll();
+		List<String> managers = users.stream()
+				.filter(u -> u.getRole().equals(Roles.MANAGER))
+				.map(m -> m.getUserId())
+				.collect(Collectors.toList());
+		
+		return managers;
+		
 	}
 }
